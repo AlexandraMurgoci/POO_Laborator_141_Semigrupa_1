5e18539a6d3e330162e4998fe783465c6fa885ee
SGA <sga@pop-os.localdomain>
2022-03-24 07:35:04 +0200

    init commit lab 6

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include "complex/ComplexTest.h"
 
 int main() {
-    ComplexTest complexTest;
-    complexTest.runAllTests();
+    //lab 2-3: Complex
+//    ComplexTest complexTest;
+//    complexTest.runAllTests();
+
+    //lab 6: Bunicuta
+
 
     return 0;
 }
